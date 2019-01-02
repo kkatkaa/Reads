37dda3879df58b70324efece963dfa067f11ab0c
Kasia <katka36@tlen.pl>
2019-01-02 11:42:55 +0100

    Add  uniqueness validation for title and isbn

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, presence: true, length: { maximum: 100 }, uniqueness: true
   validates :description, presence: true, length: { minimum: 10 }
   validates :pages, presence: true, length: { within: 1..1000 }
   validates :date, presence: true
-  validates :isbn, presence: true
+  validates :isbn, presence: true, uniqueness: true
   validates :language, presence: true
 
 end
