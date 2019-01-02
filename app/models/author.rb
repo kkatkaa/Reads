6610b84caa89fae0bff20f9f0e3686b3def7d0f5
Kasia <katka36@tlen.pl>
2019-01-02 13:32:38 +0100

    Add validation for authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :born, presence: true, length: {maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
