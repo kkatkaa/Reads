78fec6b903d6472f07ee59656fd5abff7f80bd31
Kasia <katka36@tlen.pl>
2019-01-02 11:14:20 +0100

    Add possibility to edit a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,12 +11,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def create
     @book = Book.new(params[:book].permit(:title, :description, :pages, :date, :isbn, :language))
     if @book.save
       redirect_to @book
     else
       render 'new'
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(params[:book].permit(:title, :description, :pages, :date, :isbn, :language))
+      redirect_to @book
+    else
+      render 'edit'
     end
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { minimum: 10 }
-  validates :pages, presence: true, length: { within: 4..1000 }
+  validates :pages, presence: true, length: { within: 1..1000 }
   validates :date, presence: true
   validates :isbn, presence: true
   validates :language, presence: true
