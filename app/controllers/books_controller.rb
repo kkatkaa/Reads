6f43f76027cc0bd0160c5db3eee412970db72d3d
Kasia <katka36@tlen.pl>
2019-01-02 19:06:23 +0100

    Add tag filtering

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all.order("created_at desc")
+    @books = @books.where("? = any(tags)", params[:q]) if params[:q].present?
   end
 
   def show
