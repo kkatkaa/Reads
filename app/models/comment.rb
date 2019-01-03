ba0a131f16329012888f67f568a32a0efdbd2529
Kasia <katka36@tlen.pl>
2019-01-03 21:54:03 +0100

    Create validators file and add email requiremant to model comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
+  include ActiveModel::Validations
   validates :body, presence: true, length: {in: 6..500}
   belongs_to :book
-  belongs_to :user 
+  belongs_to :user
 end
