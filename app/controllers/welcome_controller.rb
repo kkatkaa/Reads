88a0ca6fd2080cab1a4d1ae2249434c54af4aff4
Kasia <katka36@tlen.pl>
2019-01-04 11:34:32 +0100

    Do some refactoring in welcome/index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    if params[:q]
+       @books = Book.where('tag LIKE ?', "%#{params[:q]}%")
+     else
+       @books = Book.all
+     end
   end
 end
