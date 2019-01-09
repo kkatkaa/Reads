8d8ea42dbbb597665590f35696169c68a7c680ea
Kasia <katka36@tlen.pl>
2019-01-09 11:51:50 +0100

    Change validation for body in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   include ActiveModel::Validations
-  validates :body, presence: true, length: {in: 6..500}
+  validates :body, presence: true, length: {in: 6..1000}
   validates :rating, numericality: true, :inclusion => 1..10
   belongs_to :book
   belongs_to :user
