a66d4ad123c464b9e3e1968d81162174ec6449fd
Kasia <katka36@tlen.pl>
2019-01-09 21:26:00 +0100

    Add pagination to authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,6 +5,7 @@ class AuthorsController < ApplicationController
 
   def index
     @authors = Author.all.order("created_at desc")
+    @authors = Author.paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
