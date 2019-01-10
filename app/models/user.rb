98fe3b30df409bf4c818d6a6def278c62b356dfe
Kasia <katka36@tlen.pl>
2019-01-10 18:33:50 +0100

    Add avatar column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
   has_many :books, foreign_key: :user_id
-  has_many :authors, foreign_key: :user_id 
+  has_many :authors, foreign_key: :user_id
 
   def admin?
      admin
