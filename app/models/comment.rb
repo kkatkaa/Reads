fe004613a424378d2b3fe602da0d38a49bc18bad
Kasia <katka36@tlen.pl>
2019-01-12 15:22:41 +0100

    Add method to update column rating_average in books

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,13 @@
 class Comment < ApplicationRecord
+  after_save :update_rating_average
   include ActiveModel::Validations
   validates :body, presence: true, length: {in: 6..1000}
   validates :rating, numericality: true, :inclusion => 1..10
   belongs_to :book
   belongs_to :user
+
+  private
+  def update_rating_average
+    book.update_column(:rating_average, book.avg_of_grades)
+  end
 end
