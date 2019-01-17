e127c68a218b1da3bbcba9f5fd7036e274411905
Kasia <katka36@tlen.pl>
2019-01-17 19:09:19 +0100

    Create users controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'welcome/index'
-  
+
+  root 'users#index'
+  get 'users/index'
+
   resources :books do
     resources :comments
   end
